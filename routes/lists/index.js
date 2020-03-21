0faa9c7fd04af1a0c57b2c78c671931c00401bd2
Justin Carter <aws@justin-carter.co.uk>
2020-03-21 12:32:15 +0000

    added prio lists

--- routes/lists/index.js
+++ routes/lists/index.js
@@ -11,14 +11,15 @@ router.get('/', function (req, res) {
 
 });
 
-router.get('/:id', function (req, res) {
+router.get('/:listId', function (req, res) {
     let query = {
-        text: "select character_name,count(a.raid_id) as count from roster inner join attendance a on roster.character_id = a.character_id where not exists(SELECT * FROM loot INNER JOIN priority_list_items on loot.loot_id = priority_list_items.item_id where loot.loot_id = priority_list_items.item_id AND public.priority_list_items.list_id =$1 AND loot.character_id= roster.character_id) group by character_name order by count desc",
-        values: [ req.sanitize(req.params.id)]
+        text: "select character_name,character_class,count(a.raid_id) as count from roster inner join attendance a on roster.character_id = a.character_id where not exists(SELECT * FROM loot INNER JOIN priority_list_items on loot.loot_id = priority_list_items.item_id where loot.loot_id = priority_list_items.item_id AND priority_list_items.list_id =$1 AND loot.character_id= roster.character_id) AND roster.character_status =1 group by character_name,character_class order by count desc",
+        values: [ req.sanitize(req.params.listId)]
     };
     database.query(query).then((results) => {
         res.json(results.rows)
     }).catch((error) => {
+        console.log(error.message)
         res.json({})
     })
 
