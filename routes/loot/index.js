f95a9e670b655dfa35e32f89e81385b28d36c0bb
Justin Carter <aws@justin-carter.co.uk>
2020-03-25 22:41:18 +0000

    fixed bug

--- routes/loot/index.js
+++ routes/loot/index.js
@@ -5,19 +5,19 @@ let database = require('../../config/db');
 
 router.get('/', function (req, res) {
     let query;
-    if (req.query.search){
-         query = {
+    if (req.query.search) {
+        query = {
             text: 'SELECT character_name,item_id,loot_type,item_name,item_quality,character_class,loot_subcategory,character_status FROM LOOT INNER JOIN roster r on loot.character_id = r.character_id INNER JOIN items i on loot.loot_id = i.item_id INNER JOIN raids ra on loot.raid_id = ra.raid_id WHERE character_name ILIKE $1 OR item_name ILIKE $2 ORDER BY ra.raid_date DESC,ra.raid_start DESC,character_name',
-             values: ['%' + req.query.search + '%','%' + req.query.search + '%']
+            values: ['%' + req.query.search + '%', '%' + req.query.search + '%']
         };
     } else {
-         query = {
+        query = {
             text: 'SELECT character_name,item_id,loot_type,item_name,item_quality,character_class,loot_subcategory,character_status FROM LOOT INNER JOIN roster r on loot.character_id = r.character_id INNER JOIN items i on loot.loot_id = i.item_id INNER JOIN raids ra on loot.raid_id = ra.raid_id ORDER BY ra.raid_date DESC,ra.raid_start DESC,character_name'
         };
     }
 
     database.query(query).then((results) => {
-       res.json(results.rows)
+        res.json(results.rows)
     }).catch((error) => {
         res.json({})
     })
@@ -26,18 +26,18 @@ router.get('/', function (req, res) {
 
 router.post('/', function (req, res) {
     let notes;
-        if (req.params.loot_type){
-            notes = req.sanitize(req.params.loot_type)
-        } else {
-            notes = ''
-        }
+    if (req.params.notes) {
+        notes = req.sanitize(req.params.notes)
+    } else {
+        notes = ''
+    }
     const query = {
         text: 'INSERT INTO loot (character_id, raid_id, loot_id, loot_type,notes) VALUES ($1,$2,$3,$4)',
-        values: [req.sanitize(req.params.character_id),req.sanitize(req.params.raid_id),req.sanitize(req.params.loot_id),notes]
+        values: [req.sanitize(req.params.character_id), req.sanitize(req.params.raid_id), req.sanitize(req.params.loot_id), notes]
     };
 
     database.query(query).then((results) => {
-        res.json({success:true})
+        res.json({success: true})
     }).catch((error) => {
         res.json({})
     })
@@ -55,10 +55,10 @@ router.get('/recent', function (req, res) {
 
 router.get('/distribution', function (req, res) {
     let query;
-    if (req.query.search){
+    if (req.query.search) {
         query = {
-            text:"select distinct roster.character_name,roster.character_class,(select count(*) FROM loot where loot.character_id = roster.character_id AND loot_subcategory=1) AS mainspec, (select count(*) FROM loot where loot.character_id = roster.character_id AND loot_subcategory=2) as minor, (select count(*) FROM loot where loot.character_id = roster.character_id AND loot_subcategory=3) as offspec, (select count(*) FROM loot where loot.character_id = roster.character_id AND loot_subcategory=4) as resist, (select count(*) FROM attendance where attendance.character_id = roster.character_id) as attendance from roster INNER JOIN attendance a on roster.character_id = a.character_id where character_status = 1 AND (character_name ILIKE $1 OR character_class ILIKE $2) order by character_name ASC",
-            values: ['%' + req.query.search + '%','%' + req.query.search + '%']
+            text: "select distinct roster.character_name,roster.character_class,(select count(*) FROM loot where loot.character_id = roster.character_id AND loot_subcategory=1) AS mainspec, (select count(*) FROM loot where loot.character_id = roster.character_id AND loot_subcategory=2) as minor, (select count(*) FROM loot where loot.character_id = roster.character_id AND loot_subcategory=3) as offspec, (select count(*) FROM loot where loot.character_id = roster.character_id AND loot_subcategory=4) as resist, (select count(*) FROM attendance where attendance.character_id = roster.character_id) as attendance from roster INNER JOIN attendance a on roster.character_id = a.character_id where character_status = 1 AND (character_name ILIKE $1 OR character_class ILIKE $2) order by character_name ASC",
+            values: ['%' + req.query.search + '%', '%' + req.query.search + '%']
         }
     } else {
         query = "select distinct roster.character_name,roster.character_class,(select count(*) FROM loot where loot.character_id = roster.character_id AND loot_subcategory=1) AS mainspec, (select count(*) FROM loot where loot.character_id = roster.character_id AND loot_subcategory=2) as minor, (select count(*) FROM loot where loot.character_id = roster.character_id AND loot_subcategory=3) as offspec, (select count(*) FROM loot where loot.character_id = roster.character_id AND loot_subcategory=4) as resist, (select count(*) FROM attendance where attendance.character_id = roster.character_id) as attendance from roster INNER JOIN attendance a on roster.character_id = a.character_id where character_status = 1 order by character_name ASC"
