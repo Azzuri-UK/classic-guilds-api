65891d8af63343928a56a9f2c9f4803e520c41d0
Justin Carter <aws@justin-carter.co.uk>
2020-05-08 12:33:44 +0100

    added support for multiple bank chars

--- routes/bank/index.js
+++ routes/bank/index.js
@@ -14,7 +14,7 @@ const repItems = {
 }
 router.get('/', function (req, res) {
     const query = {
-        text: 'SELECT bi.item_id,bi.item_name,bi.item_quality,items.item_quality as raid_quality,quantity FROM guild_bank_items INNER JOIN bank_items bi on guild_bank_items.item_id = bi.item_id LEFT OUTER JOIN items ON guild_bank_items.item_id = items.item_id ORDER BY bi.item_name '
+        text: 'SELECT bi.item_id,bi.item_name,bi.item_quality,items.item_quality as raid_quality,quantity,bank_char,updated FROM guild_bank_items INNER JOIN bank_items bi on guild_bank_items.item_id = bi.item_id LEFT OUTER JOIN items ON guild_bank_items.item_id = items.item_id ORDER BY bi.item_name '
     };
     database.query(query).then((results) => {
         results.rows.forEach((item, index) => {
@@ -35,11 +35,14 @@ router.post('/', function (req, res) {
     let copper = 0;
     let silver = 0;
     let bankItems = {};
+    let characterName = null;
+    let updateDate = new Date();
     data.forEach((row, index) => {
         let rowData = row.replace('[', '').replace(']', '').split(',')
 
         switch (index){
             case 0:
+                characterName = rowData[0];
                 gold = rowData[1].substr(0, rowData[1].length - 4);
                 silver = rowData[1].substr(-4, 2);
                 copper = rowData[1].substr(-2, 2);
@@ -59,13 +62,14 @@ router.post('/', function (req, res) {
     let promises = [];
 
     const deleteQuery = {
-        text: 'DELETE FROM guild_bank_items',
+        text: 'DELETE FROM guild_bank_items WHERE bank_char = $1',
+        values: [characterName]
     };
     database.query(deleteQuery).then((deleteResults) => {
         Object.keys(bankItems).forEach(function(key) {
             let query = {
-                text: 'INSERT INTO guild_bank_items (item_id,quantity) VALUES ($1,$2)',
-                values: [req.sanitize(key), req.sanitize(bankItems[key])],
+                text: 'INSERT INTO guild_bank_items (item_id,quantity,bank_char,updated) VALUES ($1,$2,$3,$4)',
+                values: [req.sanitize(key), req.sanitize(bankItems[key]),characterName,updateDate],
             };
             promises.push(database.query(query))
         });
